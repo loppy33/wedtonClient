02eecc5a92bd4373b4d01e1d6fd11f0357d2496b
loppy33 <nikita.petrenko1406@gmail.com>
2024-08-21 18:42:55 +0200

    add url in ton provider and mnfst

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 
 createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://https://weedton-client.vercel.app//tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>
 )
