76ece777db48d696aadf54a1fbdd93cc4d42dd5d
loppy33 <nikita.petrenko1406@gmail.com>
2024-08-21 23:41:56 +0200

    fix icon color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
 import Friends from "./pages/Friends/Friends";
 import ConnectWallet from "./pages/ConnectWallet/ConnectWallet";
 import Tasks from "./pages/Tasks/Tasks";
@@ -10,8 +10,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="friends" element={<Friends />} />
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="friends" element={<Friends />} /> */}
+          <Route index element={<Friends />} />
           <Route path="tasks" element={<Tasks />} />
           <Route path="connectWallet" element={<ConnectWallet />} />
 
